fce8b948664d116d5db5a5ce76225a7646fd554c
Piotr <pkwiatek@gmail.com>
2021-10-27 21:14:00 +0200

    Add files via upload

---
+++ BoundariesView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fisherman
+{
+    /// <summary>
+    /// Logika interakcji dla klasy BoundariesView.xaml
+    /// </summary>
+    public partial class BoundariesView : Window
+    {
+        public BoundariesView()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private Point startPoint;
+        private Rectangle rect;
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(canvas);
+
+            rect = new Rectangle
+            {
+                Stroke = Brushes.LightBlue,
+                StrokeThickness = 2
+            };
+            Canvas.SetLeft(rect, startPoint.X);
+            Canvas.SetTop(rect, startPoint.Y);
+            canvas.Children.Add(rect);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released || rect == null)
+                return;
+
+            var pos = e.GetPosition(canvas);
+
+            var x = Math.Min(pos.X, startPoint.X);
+            var y = Math.Min(pos.Y, startPoint.Y);
+
+            var w = Math.Max(pos.X, startPoint.X) - x;
+            var h = Math.Max(pos.Y, startPoint.Y) - y;
+
+            rect.Width = w;
+            rect.Height = h;
+
+            Canvas.SetLeft(rect, x);
+            Canvas.SetTop(rect, y);
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            rect = null;
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.BoundariesVal = "test123";
+            this.Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,11 +2,11 @@
 using System.Windows;
 using System.Drawing;
 using System.Windows.Forms;
-using MessageBox = System.Windows.MessageBox;
 using Application = System.Windows.Forms.Application;
 using System.Runtime.InteropServices;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Fisherman
@@ -16,6 +16,7 @@ namespace Fisherman
     /// </summary>
     public partial class MainWindow : INotifyPropertyChanged
     {
+        CancellationTokenSource _tokenSource = null;
         private const UInt32 MOUSEEVENTF_LEFTDOWN = 0X0002;
         private const UInt32 MOUSEEVENTF_LEFTUP = 0X0004;
 
@@ -25,24 +26,58 @@ namespace Fisherman
         public MainWindow()
         {
             DataContext = this;
-            InitializeComponent();
-            
+            InitializeComponent();            
         }
         
 
         private async void StartFisherman(object sender, RoutedEventArgs e)
         {
-            await Cos1();
+            _tokenSource = new CancellationTokenSource();
+            var token = _tokenSource.Token;
+            await Cos1(token);
         }
+        private void StopFisherman(object sender, RoutedEventArgs e)
+        {
+            //updateLog("task canceled send!");
+            _tokenSource.Cancel();
+        }
+        private void TestClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show(BoundariesVal);
 
-        private bool SerchPixel(string serch1)
+        }
+        //private bool SerchPixel(string serch1)
+        //{
+        //    Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        //    //Bitmap bitmap = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+        //    Graphics graphics = Graphics.FromImage(bitmap as Image);
+        //    graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+        //    Color desirePixelColor = ColorTranslator.FromHtml(serch1);
+
+        //    for (int x = 0; x < SystemInformation.VirtualScreen.Width; x++)
+        //    {
+        //        for (int y = 0; y < SystemInformation.VirtualScreen.Height; y++)
+        //        {
+        //            Color currentPixelColor = bitmap.GetPixel(x, y);
+        //            if (desirePixelColor == currentPixelColor)
+        //            {
+        //                //MessageBox.Show(string.Format("Found Pixel at {0}, {1}", x, y));
+        //                updateLog(string.Format("Found Pixel at {0}, {1}", x, y));
+        //                return true;
+        //            }
+        //        }
+        //    }
+        //    updateLog("no Pixel found");
+        //    return false;
+        //}
+        private async Task SerchPixel(string serch1)
         {
             Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
             //Bitmap bitmap = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
             Graphics graphics = Graphics.FromImage(bitmap as Image);
             graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
             Color desirePixelColor = ColorTranslator.FromHtml(serch1);
-
+            await Task.Delay(300);
             for (int x = 0; x < SystemInformation.VirtualScreen.Width; x++)
             {
                 for (int y = 0; y < SystemInformation.VirtualScreen.Height; y++)
@@ -50,13 +85,14 @@ namespace Fisherman
                     Color currentPixelColor = bitmap.GetPixel(x, y);
                     if (desirePixelColor == currentPixelColor)
                     {
-                        MessageBox.Show(string.Format("Found Pixel at {0}, {1}", x, y));
-                        return true;
+                        //MessageBox.Show(string.Format("Found Pixel at {0}, {1}", x, y));
+                        updateLog(string.Format("Found Pixel at {0}, {1}", x, y));
+                        
                     }
                 }
-            }            
-            MessageBox.Show("no Pixel found");
-            return false;
+            }
+            updateLog("no Pixel found");
+           
         }
 
         //public async Task<bool> Run(cos)
@@ -68,27 +104,46 @@ namespace Fisherman
         //    } while (true);
         //}
 
-        public async Task Cos1()
+        public async Task Cos1(CancellationToken token)
         {
-            var inputHexColorCode = "#0CFFC4";
-            Text = inputHexColorCode + DateTime.Now.ToString();
-            logViewer.Focus();
-            logViewer.CaretIndex = logViewer.Text.Length;
-            await Task.Run(() => logViewer.ScrollToEnd());
-            //var inputHexColorCode = "#ed7d20";
-            //SerchPixel(inputHexColorCode); 
+            
+            
+                do
+                {
+                
+                    await Task.Delay(100);
+                    var inputHexColorCode = "#0EFFC5";
+                    updateLog(inputHexColorCode + " " + DateTime.Now.ToString());
+                   // await SerchPixel(inputHexColorCode);
+                if (token.IsCancellationRequested)
+                {
+                    //clean up code
+                    updateLog("task canceled !");
+                    // token.ThrowIfCancellationRequested();
+                }
+            } while (!token.IsCancellationRequested);
+            
+            
+            
+                
+                //var inputHexColorCode = "#ed7d20";
+                //SerchPixel(inputHexColorCode); 
+               // await Cos1(token);
         }
-        public void cos() {
+        public void updateLog(string logText) {
 
-            var inputHexColorCode = "#0CFFC4";
-            Text = inputHexColorCode + DateTime.Now.ToString();
-            logViewer.Focus();
+            Text = logText;
+            //logViewer.Focus();
             logViewer.CaretIndex = logViewer.Text.Length;
-            logViewer.ScrollToEnd();
-            //var inputHexColorCode = "#ed7d20";
-            //SerchPixel(inputHexColorCode); 
+            logViewer.ScrollToEnd();           
 
         }
+        private void SetBoundaries(object sender, RoutedEventArgs e)
+        {
+            BoundariesView boundariesView1 = new BoundariesView();
+            //boundariesView1.WindowStyle = "None";
+            boundariesView1.Show();
+        }       
 
         private string _text;
         public string Text
@@ -96,21 +151,39 @@ namespace Fisherman
                 get { return _text; }
                 set
                 {
-                    //if (_text != value)
-                    //{
-                        _text = _text + value + "\n";
-                        OnPropertyChanged();
+                //if (_text != value)
+                //{
+                //_text = _text + value + "\n";
+                _text = value + "\n" + _text;
+
+                OnPropertyChanged();
                     //}
                 }
                 
             }
 
+        private string _boudariesVal;
+        public string BoundariesVal
+        {
+            get { return _boudariesVal; }
+            set
+            {
+                //if (_text != value)
+                //{
+                //_text = _text + value + "\n";
+                _boudariesVal = value;
+                //System.Windows.MessageBox.Show(value);
+                OnPropertyChanged();
+                //}
+            }
+
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;      
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+        }        
     }
 }
